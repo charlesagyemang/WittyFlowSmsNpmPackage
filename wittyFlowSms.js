08195e3df9f919b4e04f9ed36e2038ca6c56bffe
Work <micnkru@gmail.com>
2020-08-23 22:28:08 +0000

    full

---
+++ wittyFlowSms.js
@@ -0,0 +1,50 @@
+const axios =  require('axios')
+
+class WTSms {
+
+	constructor(appId, appSecret){
+		this._appId = appId;
+		this._appSecret = appSecret;
+	}
+
+	get appId(){
+		return this._appId;
+	}
+
+	get appSecret() {
+		return this._appSecret;
+	}
+
+	sendSms(from, to, message){
+
+		const host = 'https://api.wittyflow.com/v1/messages/send';
+
+		const bodyToSend = {
+      from,
+      to,
+      type: '1',
+      message,
+      app_id: this._appId,
+      app_secret: this._appSecret,
+    };
+
+    return axios.post(host, bodyToSend)
+	}
+
+	sendFlashMessage(from, to, message){
+
+		const host = 'https://api.wittyflow.com/v1/messages/send';
+		
+		const bodyToSend = {
+      from,
+      to,
+      type: '0',
+      message,
+      app_id: this._appId,
+      app_secret: this._appSecret,
+    };
+
+    return axios.post(host, bodyToSend)
+	}
+	
+}
